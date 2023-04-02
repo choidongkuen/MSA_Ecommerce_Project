7e7c4480ba8ce56d70db4b3ddc7db02234340271
choidongkuen <danaver12@daum.net>
2023-04-03 03:29:46 +0900

    Feat : openfeign 라이브러리 종속성 추가 및 @EnableFeignClients 추가

--- user-api/src/main/java/com/example/UserApiApplication.java
+++ user-api/src/main/java/com/example/UserApiApplication.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
 public class UserApiApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(UserApiApplication.class,args);
     }
 }
