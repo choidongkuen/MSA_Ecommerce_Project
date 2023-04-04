e6ffa3871299793002e423188a58efab08e56ea3
choidongkuen <danaver12@daum.net>
2023-04-04 15:01:05 +0900

    Feat : TestController 구현

--- user-api/src/main/java/com/example/controller/TestController.java
+++ user-api/src/main/java/com/example/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.example.controller;
 
 import com.example.service.SendMailService;
-import feign.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
