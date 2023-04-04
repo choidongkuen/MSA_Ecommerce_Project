e0fdfa58916e350803061738e39d6f5b8396f9f5
choidongkuen <danaver12@daum.net>
2023-04-04 14:57:59 +0900

    Feat : FeignConfig 구현 및 MailgunClient 수정

--- user-api/src/main/java/com/example/client/MailgunClient.java
+++ user-api/src/main/java/com/example/client/MailgunClient.java
@@ -1,16 +1,18 @@
 package com.example.client;
 
 import com.example.mailgun.SendMailForm;
-import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "mailgun", url = "https://api.mailgun.net/v3/")
+@Qualifier("mailgun")
 public interface MailgunClient {
 
     @PostMapping("sandbox92a3501de81b4fd894cc45108c2e5e8a.mailgun.org/messages")
-    Response sendEmail(@SpringQueryMap SendMailForm form);
+    ResponseEntity<String> sendEmail(@SpringQueryMap SendMailForm form);
 
 
 }
