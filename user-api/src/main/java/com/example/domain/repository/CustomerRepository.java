49b801968cbe6da84680c796716fa443707dc9ed
choidongkuen <danaver12@daum.net>
2023-04-06 02:40:58 +0900

    Refactor : [003-EMAIL-VERIFICATION] Entity 관련 리팩토링

--- user-api/src/main/java/com/example/domain/repository/CustomerRepository.java
+++ user-api/src/main/java/com/example/domain/repository/CustomerRepository.java
@@ -1,7 +1,10 @@
 package com.example.domain.repository;
 
-import com.example.domain.entity.CustomerEntity;
+import com.example.domain.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 }
