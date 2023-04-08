de9c38427fa3bd4f30ebc385db0008d3969b0d17
choidongkuen <danaver12@daum.net>
2023-04-09 03:48:56 +0900

    Refactor : [010-PRODUCT-ADD] Repository 구현

---
+++ order-api/src/main/java/com/example/domain/repository/ProductItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.domain.repository;
+
+import com.example.domain.entity.ProductItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
+}
