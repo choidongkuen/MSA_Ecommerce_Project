9fe62a3a68d5efbe9896a0cc3eb16a2a1fc3b72c
choidongkuen <danaver12@daum.net>
2023-04-10 11:06:05 +0900

    Feat : [011-PRODUCT-ITEM-ADD] 상품 & 상품 아이템 관련 exception handler 구현

---
+++ order-api/src/main/java/com/example/exception/product/ProductExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.exception.product;
+
+import com.example.exception.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ProductExceptionHandler {
+
+    @ExceptionHandler(ProductNotExistException.class)
+    public ResponseEntity<ErrorMessage> productNotExistExceptionHandler(
+            Exception e
+    ) {
+        return ResponseEntity.badRequest()
+                .body(ErrorMessage.of(e, HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(ProductItemAlreadyExistException.class)
+    public ResponseEntity<ErrorMessage> productItemAlreadyExistExceptionHandler(
+            Exception e
+    ) {
+        return ResponseEntity.badRequest()
+                .body(ErrorMessage.of(e, HttpStatus.BAD_REQUEST));
+    }
+}
