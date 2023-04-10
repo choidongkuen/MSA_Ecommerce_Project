a24e4b86bc24db68d04e04f20942241445b66d90
choidongkuen <danaver12@daum.net>
2023-04-10 14:07:13 +0900

    Feat : [012-PRODUCT-ADD-FIX] 셀러 상품 등록 수정 완료

--- order-api/src/main/java/com/example/exception/product/ProductNotExistException.java
+++ order-api/src/main/java/com/example/exception/product/ProductNotExistException.java
@@ -1,8 +1,5 @@
 package com.example.exception.product;
 
-import lombok.Getter;
-
-@Getter
 public class ProductNotExistException extends RuntimeException {
     public ProductNotExistException(String msg) {
         super(msg);
